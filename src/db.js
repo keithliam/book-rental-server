cc671fb1cbf675f9c85d3736713b652230a7858c
keithliam <keithliamm@gmail.com>
2018-07-09 12:24:08 +0800

    Connect database

--- src/db.js
+++ src/db.js
@@ -1,4 +1,28 @@
+import mysql from 'mysql';
+
 export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
-	callback();
+	const db = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'Amazing!',
+		db: 'book_rental'
+	});
+
+	db.on('ready', () => console.log('Database is connected')).on('error', err => {
+		console.log('Error in connecting to database');
+		console.log(err.message);
+	});
+
+	db.connect(err => {
+		if (err) {
+			console.log('Error in connecting to database');
+			console.log(err.message);
+		} else {
+			console.log('Success in connecting to database');
+		}
+	});
+
+	db.query('USE book_rental');
+
+	callback(db);
 }
