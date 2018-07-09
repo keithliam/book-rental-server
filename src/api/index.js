a71ada77508c286aefb1452719b91857bd1f6135
keithliam <keithliamm@gmail.com>
2018-07-09 15:55:29 +0800

    Add get groups api

--- src/api/index.js
+++ src/api/index.js
@@ -21,6 +21,42 @@ export default ({ config, db }) => {
 		})
 	});
 
+	api.get('/groups', (req, res) => {
+		const queryString = 'SELECT a.id, a.name, b.name AS `group` FROM user a JOIN user_group b ON a.id=b.user WHERE a.source=\'facebook\'';
+		db.query(queryString, (err, rows) => {
+			if(err) {
+				console.log(err);
+				res.status(500).json({ message: 'There was a problem with the database ☹️'});
+			} else {
+				var groups = [];
+				var found;
+				for(var i = 0; i < rows.length; i++) {
+					found = false;
+					for(var j = 0; j < groups.length; j++) {
+						if(groups[j].name == rows[i].group){
+							found = true;
+							groups[j].users.push({
+								id: rows[i].id,
+								name: rows[i].name
+							});
+							break;
+						}
+					}
+					if(!found) {
+						groups.push({
+							name: rows[i].group,
+							users: [{
+								id: rows[i].id,
+								name: rows[i].name
+							}]
+						})
+					}
+				}
+				res.json({ data: groups });
+			}
+		})
+	});
+
 	// perhaps expose some API metadata at the root
 	api.post('/broadcast', (req, res) => {
 		const message = req.body.message;
